c3f53a9433e581939a5b1ebc012f0e0b08bced5a
Olumba solomon <olumbasolomon08@gmail.com>
2021-03-07 10:12:18 +0100

    Using the fetch api and json to simulate data requests from  a server. Designed with bootstrap 4.0.0, special thanks to Brad Traversy and Traversy Media

---
+++ fetch_sandbox.js
@@ -0,0 +1,76 @@
+const getTextButton = document.getElementById('getText');
+const getUsersButton = document.getElementById('getUsers');
+const getPostsButton = document.getElementById('getPosts');
+const submitButton = document.getElementById('addPost');
+let paragraph = document.getElementById('para');
+
+//create our functions
+function getText(){
+    fetch('sample.txt')
+    .then(res => res.text())
+    .then((data)=>{
+        var output = '';
+        output += `
+            <div>
+                <p>${data}</p>
+            </div>
+            `;
+        paragraph.innerHTML = output;
+    });
+}
+
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(res => res.json())
+    .then((data)=>{
+        var output = '';
+        data.forEach(post => {
+            output += `
+            <div>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            </div>
+            `;
+        });
+        paragraph.innerHTML = output;
+    });
+}
+
+function getUsers(){
+    fetch('users.json')
+    .then(res => res.json())
+    .then((data) => {
+        let output ='';
+        data.forEach(d => {
+            output += `
+            <ul class="list-group mb-3">
+                <li class="list-group-item">User ID: ${d.id}</li>
+                <li class="list-group-item">User Address: ${d.address}</li>
+                <li class="list-group-item">Username: ${d.username}</li>
+            </ul>
+            `
+        })
+        paragraph.innerHTML = output;
+    });
+}
+
+function addPost(e){
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        headers: {
+            'Accept':'application/json, text/plain. */*',
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify({ title:title, body:body})
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+//event listener 
+getTextButton.addEventListener('click', getText);
+getPostsButton.addEventListener('click', getPosts);
+getUsersButton.addEventListener('click', getUsers);
+submitButton.addEventListener('submit', addPost);
